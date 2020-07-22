21336a5ab0668bd293683170ce84a83e2f6ba940
Ryker <rajnish.kumar@byjus.com>
2020-07-22 20:32:08 +0530

    Build 0.1-beta

---
+++ precache-manifest.fabc71df1ce3483ea21f620fc5902bab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732ef5b4869d79171e24d945b26fd24b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a20e9af78914535609f0",
+    "url": "/static/js/2.aaf6c0b0.chunk.js"
+  },
+  {
+    "revision": "ea3b28b1dd7d433e409c33d71ce30398",
+    "url": "/static/js/2.aaf6c0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00958584aeea2325f056",
+    "url": "/static/js/main.ea8ea618.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
